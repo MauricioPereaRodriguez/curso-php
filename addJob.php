75310acdfc09f77c2c13505b6897aa8bdd6b4b60
Mauricio Perea Rodríguez <mperea0@misena.edu.co>
2019-03-26 22:16:56 -0500

    connect to Database from addJob

--- addJob.php
+++ addJob.php
@@ -1,5 +1,42 @@
 <?php
-echo '<pre>'.print_r($_REQUEST, true).'</pre>';
+
+require_once 'vendor/autoload.php';
+
+use App\Models\Job;
+use Illuminate\Database\Capsule\Manager as Capsule;
+// Set the event dispatcher used by Eloquent models... (optional)
+//use Illuminate\Events\Dispatcher;
+//use Illuminate\Container\Container;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => '127.0.0.1',
+    'database'  => 'coursephp',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+
+//$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+if( !empty($_POST)){
+    $job = new Job();
+    $job->title = $_POST['title'];
+    $job->description = $_POST['description'];
+    $job->save();
+}
+
 ?>
 <!doctype html>
 <html>
